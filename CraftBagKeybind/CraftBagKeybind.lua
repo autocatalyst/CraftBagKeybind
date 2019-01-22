94b5c700dc57c96284db2b124680a093e43f3ee1
autocatalyst <45957838+autocatalyst@users.noreply.github.com>
2019-01-21 21:51:09 -0800

    Add files via upload
    v0.1.5a

--- CraftBagKeybind/CraftBagKeybind.lua
+++ CraftBagKeybind/CraftBagKeybind.lua
@@ -1,48 +1,25 @@
 -- Declare top-lvl table
-local ADDON_NAME = CraftBagKeybind
+CraftBagKeybind = {}
 
--- EVENT_ADD_ON_LOADED Tell UI to create menu item in Controls.
-function CraftBagKeybind:OnAddOnLoaded(event, addonName)
+local ADDON_NAME = 'CraftBagKeybind'
+
+-- EVENT_ADD_ON_LOADED Tell UI to create menu item in Controls and func Initialize.
+local function OnAddOnLoaded(event, addonName)
   if (addonName ~= ADDON_NAME) then
       return
   end
   
-  ZO_CreateStringId("SI_BINDING_NAME_TOGGLE_AUTO-ADD_TO_CRAFT_BAG", "Toggle Auto-Add Craft Bag")
-
-  EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_PLAYER_DEACTIVATED, OnPlayerDeactivated)
+  ZO_CreateStringId("SI_BINDING_NAME_TOGGLE_AUTOADD_TO_CRAFT_BAG", "Toggle Auto-Add to Craft Bag")
   EVENT_MANAGER:UnregisterForEvent(ADDON_NAME, EVENT_ADD_ON_LOADED)
-  self:Initialize()
-end
-
--- Initialize
-
-function CraftBagKeybind:Initialize()
-  EVENT_MANAGER:UnregisterForEvent(addonName, eventCode)
-  CraftBagKeybind:ConsoleCommands()
-  CraftBagKeybind:Switch()
 end
 
 -- Binding
-
 function CraftBagKeybind:Switch()
+    d('Auto-Add to Craft Bag Toggled')
+    local newState = 1 - GetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_ADD_TO_CRAFT_BAG)
+    SetSetting(SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_ADD_TO_CRAFT_BAG, newState)
+end
 
-  <Control name="Options_Gameplay_AutoAddToCraftBag" inherits="ZO_Options_Checkbox">
-  <Anchor point="TOPLEFT" relativeTo="Options_Gameplay_UseAutoLoot_Stolen" relativePoint="BOTTOMLEFT" offsetX="-20" offsetY="4" />
-  <OnInitialized>
-      self.data = KEYBOARD_OPTIONS:GetSettingsData(SETTING_PANEL_GAMEPLAY, SETTING_TYPE_LOOT, LOOT_SETTING_AUTO_ADD_TO_CRAFT_BAG)
-      ZO_OptionsWindow_InitializeControl(self)
-  </OnInitialized>
-</Control>
-
---
- 
--- Load the addon with this
-EVENT_MANAGER:RegisterForEvent(
-    ADDON_NAME,
-    EVENT_ADD_ON_LOADED,
-    function(...)
-        CraftBagKeybind:OnAddOnLoaded(...)
-    end
-
-
+-- Load Event Manager
+EVENT_MANAGER:RegisterForEvent(ADDON_NAME, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
 --end
